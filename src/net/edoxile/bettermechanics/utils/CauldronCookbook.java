3ffdbdecc33e412c52d887f55961a53105246b42
Sleaker <sleaker@comcast.net>
2012-01-19 10:38:03 -0800

    Revert "I have no clue if the new configuration supports nested list elements like this, we'll find out."
    This reverts commit af2f489ff05ff60173c5e85cd82f0a1427b9c3f6.

--- src/net/edoxile/bettermechanics/utils/CauldronCookbook.java
+++ src/net/edoxile/bettermechanics/utils/CauldronCookbook.java
@@ -7,18 +7,12 @@ package net.edoxile.bettermechanics.utils;
 
 import net.edoxile.bettermechanics.BetterMechanics;
 import net.edoxile.bettermechanics.exceptions.KeyNotFoundException;
+import org.bukkit.util.config.Configuration;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Logger;
-
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 /**
  * Store of recipes.
@@ -28,54 +22,48 @@ import org.bukkit.configuration.file.YamlConfiguration;
 public class CauldronCookbook {
     private List<Recipe> recipes = new ArrayList<Recipe>();
     private static final Logger log = Logger.getLogger("Minecraft");
-    private FileConfiguration config;
-    private InputStream defaultConfig;
-    private File configFile;
+    private BetterMechanics instance;
+    private Configuration config;
 
     @SuppressWarnings("unchecked")
     public CauldronCookbook(BetterMechanics plugin) {
-        //Load in the configuration and it's defaults
-        configFile = new File(plugin.getDataFolder(), "cauldron-recipes.yml");
-        config = YamlConfiguration.loadConfiguration(configFile);
-        defaultConfig = plugin.getResource("cauldron-recipes.yml");
-        YamlConfiguration defaults = YamlConfiguration.loadConfiguration(defaultConfig);
-        config.setDefaults(defaults);
-        config.options().copyDefaults(true);
+        instance = plugin;
+        config = instance.getConfiguration();
         try {
-            config.save(configFile);
-        } catch (IOException e1) {
+            File configFile = new File("plugins/BetterMechanics/cauldron-recipes.yml");
+            log.info("[BetterMechanics] Loading cauldron recipes from " + configFile.getAbsolutePath());
+            config = new Configuration(configFile);
+            config.load();
+        } catch (Exception e) {
+            log.warning("[BetterMechanics] Something went wrong loading the config file.");
+            return;
         }
-
-
-        ConfigurationSection rSection = config.getConfigurationSection("recipes");
-        if (rSection != null) {
-            Set<String> recipeNames = rSection.getKeys(false);
-            if (recipeNames == null) {
-                log.warning("[BetterMechanics] Error loading cauldron recipes: no recipes found! (you probably messed up the yml format somewhere)");
+        List<String> recipeNames = config.getKeys("recipes");
+        if (recipeNames == null) {
+            log.warning("[BetterMechanics] Error loading cauldron recipes: no recipes found! (you probably messed up the yml format somewhere)");
+            return;
+        }
+        for (String name : recipeNames) {
+            MaterialMap ingredients = new MaterialMap();
+            MaterialMap results = new MaterialMap();
+            try {
+                List<List<Integer>> list = (List<List<Integer>>) config.getProperty("recipes." + name + ".ingredients");
+                for (List<Integer> l : list) {
+                    ingredients.put(l.get(0), l.get(1));
+                }
+                list = (List<List<Integer>>) config.getProperty("recipes." + name + ".results");
+                for (List<Integer> l : list) {
+                    results.put(l.get(0), l.get(1));
+                }
+            } catch (Exception e) {
+                recipes.clear();
+                log.warning("[BetterMechanics] Error loading cauldron recipes: " + e.getMessage() + "(" + e.getClass().getName() + ") (you probably messed up the yml format somewhere)");
                 return;
             }
-            for (String name : recipeNames) {
-                MaterialMap ingredients = new MaterialMap();
-                MaterialMap results = new MaterialMap();
-                try {
-                    List<List<Integer>> list = (List<List<Integer>>) rSection.get("recipes." + name + ".ingredients");
-                    for (List<Integer> l : list) {
-                        ingredients.put(l.get(0), l.get(1));
-                    }
-                    list = (List<List<Integer>>) rSection.get("recipes." + name + ".results");
-                    for (List<Integer> l : list) {
-                        results.put(l.get(0), l.get(1));
-                    }
-                } catch (Exception e) {
-                    recipes.clear();
-                    log.warning("[BetterMechanics] Error loading cauldron recipes: " + e.getMessage() + "(" + e.getClass().getName() + ") (you probably messed up the yml format somewhere)");
-                    return;
-                }
 
-                add(new Recipe(name, ingredients, results));
-            }
-            log.info("[BetterMechanics] Cauldron loaded " + size() + " recipes.");
+            add(new Recipe(name, ingredients, results));
         }
+        log.info("[BetterMechanics] Cauldron loaded " + size() + " recipes.");
     }
 
     public void add(Recipe recipe) {
